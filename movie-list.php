77b10f2d93eb1c796601a0cac5623af761de5fbc
Balram Upadhyay <JackRourkeK@gmail.com>
2021-04-19 12:25:00 +0545

    Plugin File Generated and Header Information Included alongside with License Details

---
+++ movie-list.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Plugin Name
+ *
+ * @package MovieList
+ * @author Balram Upadhyay <balram.themegrill@gmail.com>
+ * @copyright 2021 Balram Upadhyay
+ * @license GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name: Movie List
+ * Plugin URI: https://github.com/JackRourkeK/movie-list
+ * Description: Movie Listing with Custom Categories and Tags. Listing and Details Page Included.
+ * Version: 1.0.0
+ * Requires at least: 5.7.1
+ * Author: Balram Upadhyay
+ * Author URI: https://www.bru.com.np
+ * Text Domain: movie-list
+ * License: GPL v2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+/*
+Movie List is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+
+Movie List is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Movie List. If not, see {URI to Plugin License}.
+*/
+
+namespace MovieList;
+
+/**
+ * Function to Load Plugin
+ */
+function movie_list() {
+
+}
