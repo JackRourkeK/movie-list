747c76547bb237b8b02bb6aabbb0400768757b05
Balram Upadhyay <JackRourkeK@gmail.com>
2021-04-22 09:37:17 +0545

    Function for add shortcode has been separated from the inline function, Escaping for the security and Localization as well

--- includes/Classes/Short_Code.php
+++ includes/Classes/Short_Code.php
@@ -16,85 +16,87 @@ class Short_Code {
 
 	/**
 	 * Function to Generate Shortcode which can be used in pages.
-	 *
-	 * @param mixed $shortcode_parameters for mixed value.
 	 */
-	public static function generate_shortcode( $shortcode_parameters = null ) {
-		add_shortcode(
-			'bu_movie_list',
-			function ( $shortcode_parameters ) {
-				$shortcode_parameters = shortcode_atts( array( 'per_page' => 5 ), $shortcode_parameters );
-				$paged                = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
-				$movie_list_args      = array(
-					'post_type'      => 'movie-list',
-					'posts_per_page' => $shortcode_parameters['per_page'],
-					'paged'          => $paged,
-				);
+	public static function generate_shortcode() {
+		add_shortcode( 'bu_movie_list', array( self::class, 'generate_html_shortcode' ) );
+	}
 
-				$get_movie_list = get_posts( $movie_list_args );
-
-				$get_movie_count = count( get_posts( array( 'post_type' => 'movie-list' ) ) );
-
-				$result_html = '';
-
-				foreach ( $get_movie_list as $movie_list ) {
-					$get_rating_value = get_post_meta( $movie_list->ID, 'movie_rating', true );
-					$get_movie_price  = get_post_meta( $movie_list->ID, 'movie_price', true );
-					$get_movie_tags   = get_the_terms( $movie_list->ID, 'movie_type' );
-
-					$get_movie_tags_name = implode( ', ', self::get_movie_tags( $get_movie_tags ) );
-
-					$rating_value = '';
-					$movie_price  = '';
-					$movie_tags   = '';
-
-					if ( $get_rating_value > 0 ) {
-						$rating_value .= '<br/>';
-						$rating_value .= '<div class="wrapper-star">';
-						for ( $i = 1; $i <= $get_rating_value; $i++ ) {
-							$rating_value .= '<label for="r1">&#10038;</label>';
-						}
-						$rating_value .= '<br/></div>';
-					}
-					if ( $get_movie_price > 0 ) {
-						$movie_price .= '<h3> Movie Price: NRs. <u>' . $get_movie_price . '</u></h3>';
-					}
-					if ( ! empty( $get_movie_tags ) ) {
-						$movie_tags .= '<br><strong> Movie Tags: </strong>' . $get_movie_tags_name;
-					}
-					$result_html .= '<div class="entry-content"><h1><a href="' . get_permalink( $movie_list->ID ) . '">'
-					. $movie_list->post_title
-					. '</h1></a>'
-					. $movie_price
-					. get_the_post_thumbnail(
-						$movie_list->ID,
-						'post-thumbnail',
-						array(
-							'class' => 'movie-img',
-							'style' => 'width:100px; height:100px;',
-						)
-					)
-					. $rating_value
-					. $movie_tags
-					. ( ( ! empty( $movie_list->post_content ) ) ? '<h3>Movie Details: </h3>' : '' ) . $movie_list->post_content
-					. '</p></div>';
-
-				}
-				// Code to Paginate the Movie Lists.
-				$max_pages_count = ceil( ( $get_movie_count / $shortcode_parameters['per_page'] ) );
-
-				$big = 9999999999; // need an unlikely integer.
-
-				$result_html .= paginate_links(
-					array(
-						'base'    => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
-						'current' => max( 1, $paged ),
-						'total'   => $max_pages_count,
-					)
-				);
-				return $result_html;
-			}
+	/**
+	 * Function to generate html and pagination when implementing the shortcode
+	 *
+	 * @param mixed $shortcode_parameters (Parameter to define the pagination implementation for the post).
+	 */
+	public static function generate_html_shortcode( $shortcode_parameters = null ) {
+		$shortcode_parameters = shortcode_atts( array( 'per_page' => 5 ), $shortcode_parameters );
+		$paged                = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
+		$movie_list_args      = array(
+			'post_type'      => 'movie-list',
+			'posts_per_page' => $shortcode_parameters['per_page'],
+			'paged'          => $paged,
 		);
+
+		$get_movie_list = get_posts( $movie_list_args );
+
+		$get_movie_count = count( get_posts( array( 'post_type' => 'movie-list' ) ) );
+
+		$result_html = '';
+
+		foreach ( $get_movie_list as $movie_list ) {
+			$get_rating_value = get_post_meta( $movie_list->ID, 'movie_rating', true );
+			$get_movie_price  = get_post_meta( $movie_list->ID, 'movie_price', true );
+			$get_movie_tags   = get_the_terms( $movie_list->ID, 'movie_type' );
+
+			$get_movie_tags_name = implode( ', ', self::get_movie_tags( $get_movie_tags ) );
+
+			$rating_value = '';
+			$movie_price  = '';
+			$movie_tags   = '';
+
+			if ( $get_rating_value > 0 ) {
+				$rating_value .= '<br/>';
+				$rating_value .= '<div class="wrapper-star">';
+				for ( $i = 1; $i <= $get_rating_value; $i++ ) {
+					$rating_value .= '<label for="r1">&#10038;</label>';
+				}
+				$rating_value .= '<br/></div>';
+			}
+			if ( $get_movie_price > 0 ) {
+				$movie_price .= '<h3> ' . esc_html__( 'Movie Price', 'movie-list' ) . ': NRS. <u>' . $get_movie_price . '</u></h3>';
+			}
+			if ( ! empty( $get_movie_tags ) ) {
+				$movie_tags .= '<br><strong>' . esc_html__( 'Movie Tags', 'movie-list' ) . ': </strong>' . $get_movie_tags_name;
+			}
+			$result_html .= '<div class="entry-content"><h1><a href="' . get_permalink( $movie_list->ID ) . '">'
+			. $movie_list->post_title
+			. '</h1></a>'
+			. $movie_price
+			. get_the_post_thumbnail(
+				$movie_list->ID,
+				'post-thumbnail',
+				array(
+					'class' => 'movie-img',
+					'style' => 'width:100px; height:100px;',
+				)
+			)
+			. $rating_value
+			. $movie_tags
+			. ( ( ! empty( $movie_list->post_content ) ) ? '<h3>' . esc_html__( 'Movie Details', 'movie-list' ) . ': </h3>' : '' ) . $movie_list->post_content
+			. '</p></div>';
+
+		}
+		// Code to Paginate the Movie Lists.
+		$max_pages_count = ceil( ( $get_movie_count / $shortcode_parameters['per_page'] ) );
+
+		$big = 9999999999; // need an unlikely integer.
+
+		$result_html .= paginate_links(
+			array(
+				'base'    => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+				'current' => max( 1, $paged ),
+				'total'   => $max_pages_count,
+			)
+		);
+		return $result_html;
 	}
 
 	/**
