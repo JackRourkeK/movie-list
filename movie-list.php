b8a2dc162537b7703129ecde57ed20482e696496
JackRourkeK <jackrourkek@gmail.com>
2021-04-30 10:40:18 +0545

    Something Changed (#10)
    Issue has been fixed.

--- movie-list.php
+++ movie-list.php
@@ -49,7 +49,7 @@ if ( ! defined( 'BU_PLUGIN_BASENAME' ) ) {
 }
 
 /**
- * Checks if Movie_List class exists or not, if not, then create an instance of the Movie_List class (Checking for PR New Added and Rebase and Squash).
+ * Checks if Movie_List class exists or not, if not, then create an instance of the Movie_List class (Checking for PR New Added and Rebase and Squash Something Changed).
  */
 if ( ! class_exists( 'Movie_List' ) ) {
 	Movie_List::instance();
